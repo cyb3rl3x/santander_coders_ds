2ff47bb8c7b9b8cb9bfeb5e38250e54d9615f342
Alex Lima <alexlimacavaleral@gmail.com>
2023-03-03 18:05:18 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import modulo.calculadora as calc
+
+x=10
+y=20
+
+resultado = calc.adicao(x, y)
+print(resultado)
---
+++ modulo/calculadora.py
@@ -0,0 +1,12 @@
+
+def adicao(x, y):
+    return x+y
+
+def subtracao(x, y):
+    return x-y
+
+def produto(x, y):
+    return x*y
+
+def divisao(x/y):
+    return x/y
